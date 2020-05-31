13adec088b1db381e76a4454638521609285b1c9
erdahuja <er.d.ahuja@gmail.com>
2020-05-31 16:19:46 +0530

    add auth service, middleware, signup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
 const app = express();
 app.use(json());
 
